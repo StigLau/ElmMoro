23b80b22047d1a9f4f187584940bec06d77f568a
Stig Lau <stig@lau.no>
2016-11-15 19:35:06 +0100

    Example can now post

--- kompostedit/src/Models/Simple.elm
+++ kompostedit/src/Models/Simple.elm
@@ -17,41 +17,61 @@ import Json.Decode exposing (..)
 import Json.Decode.Pipeline exposing (decode, required, optional)
 import JsonDecoding exposing (..)
 import Process exposing (sleep)
+import String
 
 
 type alias Model =
   { name: String
   , start: String
   , end: String
+  , hats : List String
   }
 
 type Msg
     = Update
+    | GetFailed Http.Error
+    | GetSucceeded (List String)
 
-initModel : Model
-initModel = Model "" "" ""
+initModel : (Model, Cmd Msg)
+initModel = (Model "" "" "" [], coolestHats)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Update ->
-            model
+            (model, Cmd.none)
+
+        GetFailed _ ->
+            (model, Cmd.none)
+
+        GetSucceeded strings ->
+            ({model | hats = strings }, Cmd.none)
 
 
 view : Model -> Html Msg
-view model = text (toString model)
+view model = div [ ] [ text (String.join "," model.hats ) ]
 
 
+--hats : Task.Task Http.Error (List String)
+--hats = Http.post (list string) "http://example.com/hat-categories.json" Http.empty
 
-{--
-main : Program Never
-main = program { init = init, update = update, view = view, subscriptions = subscriptions}
---}
+coolestHats : Cmd Msg
+coolestHats = Task.perform GetFailed GetSucceeded coolestHatsRequest
+
+coolestHatsRequest : Task.Task Http.Error (List String)
+coolestHatsRequest = Http.post (list string) "http://example.com/hats"
+      (Http.string """{ "sortBy": "coolness", "take": 10 }""")
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
+
 main : Program Never
 main =
-    App.beginnerProgram
-        { model = initModel
+    App.program
+        { init = initModel
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
