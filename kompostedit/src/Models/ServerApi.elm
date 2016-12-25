b56535f06347c58e2def82f41fb690543666f083
Stig Lau <stig.lau@evry.com>
2016-12-25 21:38:02 +0100

    At last, got fetch from HTTP/Json working with 0.18

---
+++ kompostedit/src/Models/ServerApi.elm
@@ -0,0 +1,193 @@
+module Models.ServerApi exposing (..)
+
+import Json.Decode as JsonD
+import Json.Encode as JsonE
+import Http
+
+
+type alias ArtistRequest a =
+    { a | name : String }
+
+
+type alias Artist =
+    { id : Int
+    , name : String
+    }
+
+
+type alias AlbumRequest a =
+    { a
+        | name : String
+        , artistId : Int
+        , tracks : List Track
+    }
+
+
+type alias Album =
+    { id : Int
+    , name : String
+    , artistId : Int
+    , tracks : List Track
+    }
+
+
+type alias Track =
+    { name : String
+    , duration : Int
+    }
+
+
+baseUrl : String
+baseUrl =
+    "http://localhost:8081"
+
+
+
+getArtist : Int -> (Result Http.Error Artist -> msg) -> Cmd msg
+getArtist id msg =
+    Http.get (baseUrl ++ "/artists/" ++ toString id) artistDecoder
+        |> Http.send msg
+
+
+
+getArtists : (Result Http.Error (List Artist) -> msg) -> Cmd msg
+getArtists msg =
+    Http.get (baseUrl ++ "/artists") artistsDecoder
+        |> Http.send msg
+
+
+
+
+createArtist : ArtistRequest a -> (Result Http.Error Artist -> msg) -> Cmd msg
+createArtist artist msg =
+    Http.post
+        (baseUrl ++ "/artists")
+        (Http.stringBody "application/json" <| encodeArtist artist)
+        artistDecoder
+        |> Http.send msg
+
+
+
+
+updateArtist: Artist -> (Result Http.Error Artist -> msg) -> Cmd msg
+updateArtist artist msg =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = baseUrl ++ "/artists/" ++ toString artist.id
+        , body = Http.stringBody "application/json" <| encodeArtist artist
+        , expect = Http.expectJson artistDecoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+        |> Http.send msg
+
+
+
+
+deleteArtist : Int -> (Result Http.Error String -> msg) -> Cmd msg
+deleteArtist id  msg =
+    Http.request
+        { method = "DELETE"
+        , headers = []
+        , url = baseUrl ++ "/artists/" ++ toString id
+        , body = Http.emptyBody
+        , expect = Http.expectString
+        , timeout = Nothing
+        , withCredentials = False
+        }
+        |> Http.send msg
+
+
+artistsDecoder : JsonD.Decoder (List Artist)
+artistsDecoder =
+    JsonD.list artistDecoder
+
+
+artistDecoder : JsonD.Decoder Artist
+artistDecoder =
+    JsonD.map2 Artist
+        (JsonD.field "artistId" JsonD.int)
+        (JsonD.field "artistName" JsonD.string)
+
+
+encodeArtist : ArtistRequest a -> String
+encodeArtist a =
+    JsonE.encode 0 <|
+        JsonE.object
+            [ ( "artistName", JsonE.string a.name )
+            ]
+
+
+getAlbum : Int -> (Result Http.Error Album -> msg) -> Cmd msg
+getAlbum id msg =
+    Http.get (baseUrl ++ "/albums/" ++ toString id) albumDecoder
+        |> Http.send msg
+
+
+getAlbumsByArtist : Int -> (Result Http.Error (List Album) -> msg) -> Cmd msg
+getAlbumsByArtist artistId msg =
+    Http.get (baseUrl ++ "/albums?artistId=" ++ toString artistId) albumsDecoder
+        |> Http.send msg
+
+
+
+createAlbum : AlbumRequest a -> (Result Http.Error Album -> msg) -> Cmd msg
+createAlbum album msg =
+    Http.post
+        (baseUrl ++ "/albums")
+        (Http.stringBody "application/json" <| encodeAlbum album)
+        albumDecoder
+        |> Http.send msg
+
+updateAlbum: Album -> (Result Http.Error Album -> msg) -> Cmd msg
+updateAlbum album msg =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = baseUrl ++ "/albums/" ++ toString album.id
+        , body = Http.stringBody "application/json" <| encodeAlbum album
+        , expect = Http.expectJson albumDecoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+        |> Http.send msg
+
+
+albumsDecoder : JsonD.Decoder (List Album)
+albumsDecoder =
+    JsonD.list albumDecoder
+
+
+albumDecoder : JsonD.Decoder Album
+albumDecoder =
+    JsonD.map4 Album
+        (JsonD.field "albumId"  JsonD.int)
+        (JsonD.field "albumName"  JsonD.string)
+        (JsonD.field "albumArtistId"  JsonD.int)
+        (JsonD.field "albumTracks" <| JsonD.list trackDecoder)
+
+
+trackDecoder : JsonD.Decoder Track
+trackDecoder =
+    JsonD.map2 Track
+        (JsonD.field "trackName" JsonD.string)
+        (JsonD.field "trackDuration" JsonD.int)
+
+
+encodeAlbum : AlbumRequest a -> String
+encodeAlbum album =
+    JsonE.encode 0 <|
+        JsonE.object
+            [ ( "albumName", JsonE.string album.name )
+            , ( "albumArtistId", JsonE.int album.artistId )
+            , ( "albumTracks", JsonE.list <| List.map encodeTrack album.tracks )
+            ]
+
+
+encodeTrack : Track -> JsonE.Value
+encodeTrack track =
+    JsonE.object
+        [ ( "trackName", JsonE.string track.name )
+        , ( "trackDuration", JsonE.int track.duration )
+        ]
--- kompostedit/src/Models/Simple.elm
+++ kompostedit/src/Models/Simple.elm
@@ -1,11 +1,9 @@
 module Models.Simple exposing (..)
 
-import Html.App as App
-import Html.App exposing (program, map)
 import List exposing (length)
 import Html exposing (..)
 import Html.Attributes exposing (class, classList)
-import Json.Decode exposing (Decoder, andThen, succeed, list, string, object1, fail, (:=))
+import Json.Decode exposing (..)
 import Time exposing (Time, second, millisecond)
 import Task
 import Html exposing (..)
@@ -14,54 +12,81 @@ import Http
 import Json.Encode
 import Json.Decode
 import Json.Decode exposing (..)
-import Json.Decode.Pipeline exposing (decode, required, optional)
 import JsonDecoding exposing (..)
 import Process exposing (sleep)
-import String
-
+import Models.ServerApi exposing (..)
 
 type alias Model =
-  { name: String
-  , start: String
-  , end: String
-  , hats : List String
-  }
+    { name : String
+    , start : Int
+    , end : String
+    , hats : List String
+    }
+
 
 type Msg
     = Update
     | GetFailed Http.Error
     | GetSucceeded (List String)
+    | ShowArtist (Result Http.Error Artist)
 
-initModel : (Model, Cmd Msg)
-initModel = (Model "" "" "" [], (storeKompost (storeKompostRequest "http://localhost:9099/store/kompost" "Send this" ) ))
+type alias Artist =
+    { id : Int
+    , name : String
+    }
 
-storeKompost : Task.Task Http.Error (List String) -> Cmd Msg
-storeKompost request = Task.perform GetFailed GetSucceeded request
 
+initModel : ( Model, Cmd Msg )
+-- initModel = ( Model "" "" "" [], (storeKompost (storeKompostRequest "http://localhost:9099/store/kompost" "Send this")) )
+initModel = (Model "" -2 "" [], getArtist 1 ShowArtist)
+
+-- storeKompost : Task.Task Http.Error (List String) -> Cmd Msg
+-- storeKompost request = Task.perform GetFailed GetSucceeded request
+
+{--
 storeKompostRequest : String -> String -> Task.Task Http.Error (List String)
-storeKompostRequest requestString destination = Http.post (list string) destination (Http.string requestString)
+storeKompostRequest requestString destination =
+    Http.post (list string) destination (Http.stringBody requestString)
+--}
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Update ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
 
         GetFailed _ ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
 
         GetSucceeded strings ->
-            ({model | hats = strings }, Cmd.none)
+            ( { model | hats = strings }, Cmd.none )
+
+        ShowArtist res ->
+            case res of
+                Result.Ok artist ->
+                    ( { model
+                        | name = artist.name
+                        , start = artist.id
+                      } , Cmd.none )
+
+                Result.Err err ->
+                    let _ = Debug.log "Error retrieving artist" err
+                    in
+                        (model, Cmd.none)
+
 
 view : Model -> Html Msg
-view model = div [ ] [ text (String.join "," model.hats ) ]
+view model =
+    div [] [ text (model.name ++ String.join "," model.hats) ]
+
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+    Sub.none
 
-main : Program Never
+main : Program Never Model Msg
 main =
-    App.program
+    program
         { init = initModel
         , view = view
         , update = update
