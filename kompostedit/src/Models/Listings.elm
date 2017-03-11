45cd3dfcba9e6cd85a9026ad514c0e71438e9894
Stig Lau <stig.lau@avella.no>
2017-03-11 15:16:50 +0100

    Added total rows and offset

--- kompostedit/src/Models/Listings.elm
+++ kompostedit/src/Models/Listings.elm
@@ -4,23 +4,27 @@ import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (class)
 import Http
-import Models.KompostApi exposing (getKompo, Komposition)
+import Json.Decode as JsonD
 
 
 type alias Model = {
-    something: String
+    rows: Int,
+    offset: Int
     }
 
 type Msg =
-    FetchKompostResponseHandler (Result Http.Error Komposition)
+    FetchKompostResponseHandler (Result Http.Error DvlRef)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         FetchKompostResponseHandler res ->
             case res of
-                Result.Ok komposition ->
-                     ( model, Cmd.none )
+                Result.Ok dvlRef ->(
+                    { model
+                    | rows = dvlRef.rows
+                    , offset = dvlRef.offset
+                    }, Cmd.none )
 
                 Result.Err err ->
                      let _ = Debug.log "Error retrieving komposition" err
@@ -33,7 +37,7 @@ view : Model -> Html Msg
 view model =
     div [ class "listings" ]
         [ h1 [] [ text "Listing stuff" ]
-        , div [] [ text "Komposition: " ]
+        , div [] [ text "What I get: " ]
         , text (toString model)
         , div [] [ ]
         ]
@@ -41,7 +45,7 @@ view model =
 
 
 init : ( Model, Cmd Msg )
-init = ( Model "Somethingss", (getKompo "Init" FetchKompostResponseHandler) )
+init = ( Model -1 -2, (listDvlIds FetchKompostResponseHandler) )
 
 
 subscriptions : Model -> Sub Msg
@@ -49,3 +53,25 @@ subscriptions model = Sub.none
 
 main : Program Never Model Msg
 main = program { init = init, update = update, view = view, subscriptions = subscriptions}
+
+
+listDvlIds : (Result Http.Error DvlRef -> msg) -> Cmd msg
+listDvlIds msg = Http.get ("http://heap.kompo.st/_all_docs") dvlRefDecoder
+        |> Http.send msg
+
+dvlRefDecoder : JsonD.Decoder DvlRef
+dvlRefDecoder =
+            JsonD.map2 DvlRef
+                           (JsonD.field "total_rows" JsonD.int)
+                           (JsonD.field "offset" JsonD.int)
+--                           (JsonD.field "mediaFile" mediaFileDecoder)
+--                           (JsonD.field "segments" <| JsonD.list segmentDecoder)
+                -- _ = Debug.log "testing out stuff" komp
+
+type alias DvlRef =
+  { rows: Int
+  , offset: Int
+  --, config: Config
+  --, mediaFile: Mediafile
+  --, segments: List Segment
+  }
