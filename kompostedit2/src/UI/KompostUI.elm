4f110ad26b8973a0df40b1cbd56c0d69cc631bb7
n636552 <stig.lau@nrk.no>
2017-07-06 10:22:13 +0200

    Simple GUI refreshing

--- kompostedit2/src/UI/KompostUI.elm
+++ kompostedit2/src/UI/KompostUI.elm
@@ -10,9 +10,8 @@ import RemoteData exposing (RemoteData(..))
 import Segment.SegmentUI exposing (showSegmentList)
 import UI.DvlSpecificsUI exposing (showSpecifics)
 import Models.BaseModel exposing (Model)
-import Models.Msg exposing (Msg(NavigateTo, StoreKomposition, DeleteKomposition, SegmentMsg, CreateSegment))
+import Models.Msg exposing (Msg(NavigateTo, StoreKomposition, DeleteKomposition, SegmentMsg, CreateSegment, MorePlease))
 import Navigation.AppRouting exposing (Page(Listings))
-
 
 
 kompost : Model -> Html Models.Msg.Msg
@@ -23,6 +22,10 @@ kompost model =
         , UI.DvlSpecificsUI.showSpecifics model.kompost
         , Html.map SegmentMsg(Segment.SegmentUI.showSegmentList model.kompost.segments)
         , Grid.simpleRow [ Grid.col [] [Button.button [ Button.primary, Button.small, Button.onClick CreateSegment ] [ text "New Segment" ]]]
-        , Grid.row [] [ Grid.col [] [] , Grid.col [] [], Grid.col [] [Button.button [ Button.success, Button.small, Button.onClick StoreKomposition ] [ text "Store Komposition" ]] ]
-        , Grid.row [] [ Grid.col [] [] , Grid.col [] [], Grid.col [] [Button.button [ Button.danger, Button.small, Button.onClick DeleteKomposition ] [ text "Delete Komposition" ]] ]
+        , Grid.simpleRow [ Grid.col [] [Button.button [ Button.primary, Button.small, Button.onClick MorePlease ] [ text "EvaluateMd5" ]]]
+        , Grid.simpleRow
+            [ Grid.col []
+            , Grid.col [Button.button [ Button.success, Button.small, Button.onClick StoreKomposition ] [ text "Store Komposition" ]]
+            , Grid.col [Button.button [ Button.danger, Button.small, Button.onClick DeleteKomposition ] [ text "Delete Komposition" ]]
+            ]
         ]
