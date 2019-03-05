45f3782f93d0e8c2e4ac77e4a08fb87af5fa0a48
kh1115 <kh1115@ruter.no>
2019-03-05 13:56:57 +0100

    Now supports sending some headers

--- kompost-proxy/src/main/java/Proxy.java
+++ kompost-proxy/src/main/java/Proxy.java
@@ -1,11 +1,15 @@
+import okhttp3.*;
 import org.apache.camel.*;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.language.MethodCallExpression;
 import org.eclipse.jetty.http.HttpStatus;
-
 import java.io.IOException;
 import java.net.ConnectException;
+import java.util.List;
+import java.util.Map;
+import static org.apache.camel.Exchange.CONTENT_TYPE;
+import static org.apache.camel.Exchange.HTTP_PATH;
 
 class Proxy {
     public static void main(String[] args) throws Exception {
@@ -42,23 +46,46 @@ class Routes extends RouteBuilder {
                 .setBody(new MethodCallExpression(new ElmFiles("src")));
 
         from("jetty:http://0.0.0.0:8001/kompost/?matchOnUriPrefix=true")
-                .setBody(new MethodCallExpression(new KompostFiles()))
-                .removeHeaders("*");
+                .process(fetchFilesAdapter())
+                .removeHeaders("*", CONTENT_TYPE, "ETag")
+        ;
 
         restConfiguration().port(8080);
         rest("/src").get("/{file}").produces("text/html")
                 .route()
                 .setBody(new MethodCallExpression(new ElmFiles("src")))
                 .removeHeaders("*");
+    }
 
-        rest("/kompost").consumes("application/json").produces("application/json")
-                .get("/{file}").route()
-                .setBody(new MethodCallExpression(new KompostFiles()))
-        ;
+    private Processor fetchFilesAdapter() {
+        return exchange -> {
+            Message in = exchange.getIn();
+            String httpMethod = in.getHeader(Exchange.HTTP_METHOD, String.class);
+            String theFile = in.getHeader(Exchange.HTTP_PATH, String.class);
+            String body = in.getBody(String.class);
+            Response result = FetchFiles(theFile, httpMethod, body);
+            if(result.networkResponse().isSuccessful()) {
+                Message out = exchange.getOut();
+                out.setBody(result.body().string());
+                Map<String, List<String>> headers = result.headers().toMultimap();
+                for (String headerName : headers.keySet()) {
+                    out.setHeader(headerName, headers.get(headerName));
+                }
+            }
+            result.body().close();
+            result.close();
+        };
+    }
 
-        rest("/kompost").post("/{file}").route()
-                .setBody(new MethodCallExpression(new KompostFiles()))
-        ;
+    private Response FetchFiles(@Header(HTTP_PATH) String theFile, @Header(Exchange.HTTP_METHOD) String httpMethod, @Body String body) throws IOException {
+        Request.Builder requestBuilder = new Request.Builder();
+        if (httpMethod.equals("POST")) {
+            requestBuilder = requestBuilder.post(RequestBody.create(MediaType.parse("application/json"), body));
+        }
+        requestBuilder.header("Authorization", KompostFiles.auth)
+                .url(KompostFiles.url +theFile);
+        OkHttpClient client = new OkHttpClient();
+        return client.newCall(requestBuilder.build()).execute();
     }
 }
 
