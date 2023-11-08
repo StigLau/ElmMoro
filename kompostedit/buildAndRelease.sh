afdcab3ab96e26af659bd6e6879b966e0aa6cfe3
stiglau <stig@lau.no>
2023-11-08 10:18:41 +0100

    Functional for pushing

--- kompostedit/buildAndRelease.sh
+++ kompostedit/buildAndRelease.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-elm make src/Main.elm --output release/content/elm/kompost.js
-echo "Finished building. Pushing to https://app.kompo.st/index.html"
-cd release
-terraform apply
-cd ..
-echo "Finished"
+echo "Building and releasing to kompo.se"
+elm make src/CustomerMedia/FileUpload.elm --output release/content/elm/fileupload.js && \
+elm make src/Main.elm --output release/content/elm/kompost.js && \
+aws s3 cp release/content/elm s3://api.kompo.se/elm/ --recursive
+#elm reactor
